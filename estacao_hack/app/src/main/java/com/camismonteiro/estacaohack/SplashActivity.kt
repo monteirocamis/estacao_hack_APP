25adaac8699576585a3540e7145e5835506a381b
camila monteiro <monteiro.camis@gmail.com>
2020-08-27 04:31:34 -0300

    app splash e muda tela

---
+++ estacao_hack/app/src/main/java/com/camismonteiro/estacaohack/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.camismonteiro.estacaohack
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //é um dos metodos presentes no ciclo de vida da activity
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //abri a mainActivity apos 5 segundos
+        Handler(Looper.getMainLooper()).postDelayed({
+            //iniciar uma intent - transicao da tela splash para a main
+            val mIntent = Intent(this, MainActivity::class.java)
+            startActivity(mIntent)
+            finish() // aparece 1x só
+        }, 5000)
+    }
+}
